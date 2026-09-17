6848fec160902895ce944d1e952e800273aa8470
RevaVlad <revavlad205@gmail.com>
2026-09-17 20:46:40 +0500

    feat: улучшил верстку

--- task-board/src/app/services/task-service.ts
+++ task-board/src/app/services/task-service.ts
@@ -3,34 +3,36 @@ import { Task } from '../types/task.model';
 
 @Service()
 export class TaskService {
-    readonly tasks = signal<Task[]>([
-        {
-            id: 1,
-            title: "Задача 1",
-            done: false,
-            createdAt: new Date(),
-        },
-        {
-            id: 2,
-            title: "Задача 2",
-            done: false,
-            createdAt: new Date(),
-        },
-        {
-            id: 3,
-            title: "Задача 3",
-            done: false,
-            createdAt: new Date(),
-        },
-    ]);
+  readonly tasks = signal<Task[]>([
+    {
+      id: 1,
+      title: 'Прочитать лекцию',
+      done: false,
+      createdAt: new Date(),
+    },
+    {
+      id: 2,
+      title: 'Создать проект через ng new',
+      done: false,
+      createdAt: new Date(),
+    },
+    {
+      id: 3,
+      title: 'Выспаться',
+      done: false,
+      createdAt: new Date(),
+    },
+  ]);
 
-    toggle(id: number) {
-        this.tasks.update(tasks => this.tasks().map((t) => {
-            if (t.id === id) {
-                t.done = !t.done;
-            }
-            
-            return t;
-        }));
-    }
+  toggle(id: number) {
+    this.tasks.update((tasks) =>
+      this.tasks().map((t) => {
+        if (t.id === id) {
+          t.done = !t.done;
+        }
+
+        return t;
+      }),
+    );
+  }
 }
