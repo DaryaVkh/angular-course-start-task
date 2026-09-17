910fda9c553c1cd325afb65b7339819deb33db18
RevaVlad <vladreva57@gmail.com>
2026-09-17 20:40:53 +0500

    feat: улучшил верстку

--- task-board/src/app/components/task-item/task-item.component.ts
+++ task-board/src/app/components/task-item/task-item.component.ts
@@ -5,13 +5,76 @@ import { TimeAgoPipe } from '../../pipes/time-ago-pipe';
 @Component({
   imports: [TimeAgoPipe],
   selector: 'app-task-item',
-  styles: ``,
-  template: `<div>
-    <h3>{{ task().title }}</h3>
-    <p>{{ task().done }}</p>
-    <p class="date">{{ task().createdAt | timeAgo }}</p>
-    <input type="checkbox" [checked]="task().done" (change)="toggled.emit(task().id)" />
-  </div>`,
+  styles: `
+    .task {
+      background: var(--surface);
+      outline: 2px solid var(--border-color);
+      border-radius: var(--border-radius);
+      margin-bottom: 16px;
+      padding: 20px;
+      padding-right: 60px;
+      display: block;
+      box-sizing: border-box;
+      min-width: 300px;
+      position: relative;
+    }
+
+    .task:hover {
+      outline-color: var(--accent);
+    }
+
+    .task--done {
+      outline-color: var(--accent);
+    }
+
+    .task--done:hover {
+      outline-color: var(--accent);
+    }
+
+    .task__title {
+      margin: 0;
+      margin-bottom: 8px;
+      font-size: 32px;
+      position: relative;
+      width: fit-content;
+    }
+
+    .task__title::after {
+      content: '';
+      position: absolute;
+      width: 0;
+      height: 2px;
+      background: var(--primary);
+      top: 50%;
+      left: 0;
+      transition: width 0.3s ease-out;
+    }
+
+    .task__title--done::after {
+      width: 100%;
+    }
+
+    .task__checkbox {
+      cursor: pointer;
+      position: absolute;
+      right: 20px;
+      top: 50%;
+      transform: scale(2) translate(0, -50%);
+      accent-color: var(--accent);
+    }
+  `,
+  template: `
+    <div class="task" [class.task--done]="task().done">
+      <h3 class="task__title" [class.task__title--done]="task().done">{{ task().title }}</h3>
+      <time [attr.datetime]="task().createdAt.toISOString()">{{ task().createdAt | timeAgo }}</time>
+      <input
+        class="task__checkbox"
+        type="checkbox"
+        [checked]="task().done"
+        (change)="toggled.emit(task().id)"
+      />
+    </div>
+  `,
 })
 export class TaskItemComponent {
   readonly task = input.required<Task>();
