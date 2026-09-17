1ae307f4b5b729ca605c8f0d6b3e584ef1170877
RevaVlad <vladreva57@gmail.com>
2026-09-17 20:37:17 +0500

    feat: добавил фильтрацию

--- task-board/src/app/components/task-list/task-list.component.ts
+++ task-board/src/app/components/task-list/task-list.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { TaskService } from '../../services/task-service';
 import { TaskItemComponent } from '../task-item/task-item.component';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 
 @Component({
-  imports: [TaskItemComponent],
+  imports: [TaskItemComponent, SearchBarComponent],
   selector: 'app-task-list',
   styleUrl: './task-list.component.scss',
   templateUrl: './task-list.component.html',
@@ -11,4 +12,11 @@ import { TaskItemComponent } from '../task-item/task-item.component';
 export class TaskListComponent {
   readonly taskService = inject(TaskService);
   readonly tasks = this.taskService.tasks;
+  readonly term = signal<string>('');
+
+  readonly fliteredTasks = computed(() =>
+    this.term()
+      ? this.tasks().filter((t) => t.title.toLowerCase().includes(this.term().toLowerCase()))
+      : this.tasks(),
+  );
 }
