05823c90f0ee42dcf739c37bcc444b8afd815d34
RevaVlad <revavlad205@gmail.com>
2026-09-17 20:47:14 +0500

    refactor: перенести подсветку из пайпа в компонент

--- task-board/src/app/components/highlighted-text/highlighted-text.component.ts
+++ task-board/src/app/components/highlighted-text/highlighted-text.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { HighlightedTextToken } from '../../types/highlighted-text-token';
 
 @Component({
@@ -7,11 +7,21 @@ import { HighlightedTextToken } from '../../types/highlighted-text-token';
   styles: ``,
   template: `
     @for (token of tokens(); track $index) {
-      @if (token.isHighlighted) {<mark>{{ token.text }}</mark>}
+      @if (token.isHighlighted) {<mark>{{ token.text }}</mark> }
       @else {{{ token.text }}}
     }
   `,
 })
 export class HighlightedTextComponent {
-  readonly tokens = input.required<HighlightedTextToken[]>();
+  readonly text = input.required<string>();
+  readonly highlightTerm = input.required<string>();
+
+  readonly tokens = computed(() => {
+    if (this.highlightTerm() === '') return [{ text: this.text(), isHighlighted: false }];
+    const regex = new RegExp(`(${this.highlightTerm()})`, 'gi');
+
+    return this.text()
+      .split(regex)
+      .map<HighlightedTextToken>((text) => ({ text, isHighlighted: regex.test(text) }));
+  });
 }
