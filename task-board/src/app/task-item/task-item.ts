cf0a28c2d04e9a187496d16de82e11c65dd07b0a
nnatttaly <dmitrievan730@gmail.com>
2026-09-18 18:26:59 +0500

    feat: дописана функциональность

--- task-board/src/app/task-item/task-item.ts
+++ task-board/src/app/task-item/task-item.ts
@@ -1,10 +1,34 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { Task } from '../task.model';
+import { TimeAgoPipe } from '../time-ago-pipe';
 
 @Component({
   selector: 'app-task-item',
-  imports: [],
-  template: ` <p>task-item works!</p> `,
-  styles: ``,
+  imports: [TimeAgoPipe],
+  template: `
+    <label class="row">
+      <input type="checkbox" [checked]="task().done" (change)="toggled.emit(task().id)" />
+      <span class="title">{{ task().title }}</span>
+      <span class="date">{{ task().createdAt | timeAgo }}</span>
+    </label>
+  `,
+  styles: `
+    .row {
+      display: flex;
+      align-items: center;
+      gap: 6px;
+    }
+    .title {
+      font-size: 16px;
+    }
+    .date {
+      color: gray;
+      font-size: 13px;
+    }
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TaskItem {}
+export class TaskItem {
+  readonly task = input.required<Task>();
+  readonly toggled = output<number>();
+}
--- task-board/src/app/time-ago-pipe.ts
+++ task-board/src/app/time-ago-pipe.ts
@@ -1,10 +1,43 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+function pluralize(count: number, one: string, few: string, many: string): string {
+  const mod10 = count % 10;
+  const mod100 = count % 100;
+
+  if (mod100 >= 11 && mod100 <= 14) {
+    return many;
+  }
+  if (mod10 === 1) {
+    return one;
+  }
+  if (mod10 >= 2 && mod10 <= 4) {
+    return few;
+  }
+  return many;
+}
+
 @Pipe({
   name: 'timeAgo',
 })
 export class TimeAgoPipe implements PipeTransform {
   transform(date: Date): string {
-    return '5 минут назад';
+
+    const diffSeconds = Math.floor((Date.now() - date.getTime()) / 1000);
+    if (diffSeconds < 60) {
+      return `${diffSeconds} ${pluralize(diffSeconds, 'секунду', 'секунды', 'секунд')} назад`;
+    }
+
+    const diffMinutes = Math.floor(diffSeconds / 60);
+    if (diffMinutes < 60) {
+      return `${diffMinutes} ${pluralize(diffMinutes, 'минуту', 'минуты', 'минут')} назад`;
+    }
+
+    const diffHours = Math.floor(diffMinutes / 60);
+    if (diffHours < 24) {
+      return `${diffHours} ${pluralize(diffHours, 'час', 'часа', 'часов')} назад`;
+    }
+
+    const diffDays = Math.floor(diffHours / 24);
+    return `${diffDays} ${pluralize(diffDays, 'день', 'дня', 'дней')} назад`;
   }
 }
